0bd7882749a02e39e0a8e2a29143885b0888f4ef
larry <larry@larrys-MacBook-Pro.local>
2018-09-18 22:20:45 +0800

    fix

--- 542_01_Matrix/test.go
+++ 542_01_Matrix/test.go
@@ -1,133 +1,127 @@
-package main
+func updateMatrix(m [][]int) [][]int {
+	const MaxInt = 999999999
 
-import (
-	"fmt"
-	"math/rand"
-)
-
-const MaxInt = 999999999
-
-func steps(x1, y1, x2, y2 int) int {
-	abs := func(a, b int) int {
-		c := a - b
-		if c < 0 {
-			return -c
+	max := func(n1, n2 int) int {
+		if n1 >= n2 {
+			return n1
 		}
-		return c
+		return n2
 	}
-	return abs(x1, x2) + abs(y1, y2)
-}
 
-func minSteps(m [][]int, x, y int) int {
-	minSteps := 100000
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m[i]); j++ {
-			if 0 == m[i][j] {
-				steps := steps(x, y, i, j)
-				if steps < minSteps {
-					minSteps = steps
+	steps := func(x1, y1, x2, y2 int) int {
+		abs := func(a, b int) int {
+			c := a - b
+			if c < 0 {
+				return -c
+			}
+			return c
+		}
+		return abs(x1, x2) + abs(y1, y2)
+	}
+
+	containsZero := func(sumGraph [][]int, x, y, radius int) bool {
+		xMin, yMin, xMax, yMax := x-radius-1, y-radius-1, x+radius, y+radius
+		if xMax > len(sumGraph)-1 {
+			xMax = len(sumGraph) - 1
+		}
+		if yMax > len(sumGraph[0])-1 {
+			yMax = len(sumGraph[0]) - 1
+		}
+
+		sumAllNotZeros := MaxInt
+		sum := sumAllNotZeros
+		if xMin < 0 && yMin < 0 {
+			sumAllNotZeros = (xMax + 1) * (yMax + 1)
+			sum = sumGraph[xMax][yMax]
+		} else if xMin < 0 && yMin >= 0 {
+			sumAllNotZeros = (xMax + 1) * (yMax - yMin)
+			sum = sumGraph[xMax][yMax] - sumGraph[xMax][yMin]
+		} else if xMin >= 0 && yMin < 0 {
+			sumAllNotZeros = (xMax - xMin) * (yMax + 1)
+			sum = sumGraph[xMax][yMax] - sumGraph[xMin][yMax]
+		} else {
+			sumAllNotZeros = (xMax - xMin) * (yMax - yMin)
+			sum = sumGraph[xMax][yMax] - sumGraph[xMin][yMax] - sumGraph[xMax][yMin] + sumGraph[xMin][yMin]
+		}
+		return sum < sumAllNotZeros
+	}
+
+	findMinStepsAround := func(m [][]int, x, y, radius int) int {
+		xMin, yMin, xMax, yMax := x-radius, y-radius, x+radius, y+radius
+		if xMin < 0 {
+			xMin = 0
+		}
+		if yMin < 0 {
+			yMin = 0
+		}
+		if xMax > len(m)-1 {
+			xMax = len(m) - 1
+		}
+		if yMax > len(m[0])-1 {
+			yMax = len(m[0]) - 1
+		}
+		minSteps := MaxInt
+		for i := xMin; i <= xMax; i++ {
+			for j := yMin; j <= yMax; j++ {
+				if 0 == m[i][j] {
+					steps := steps(x, y, i, j)
+					if steps < minSteps {
+						minSteps = steps
+					}
 				}
 			}
 		}
+		return minSteps
 	}
-	return minSteps
-}
 
-func findMinSteps(m [][]int, x, y int, radius int) int {
-	minSteps := MaxInt
-	for i := x - radius; i >= 0 && i <= x+radius && x < len(m); i++ {
-		for j := y - radius; j >= 0 && j <= y+radius && y < len(m[i]); j++ {
-			if 0 == m[i][j] {
-				minSteps = steps(x, y, i, j)
+	findMinStepsWithScalingRadius := func(m, sumGraph [][]int, x, y int) (int, int) {
+		minRadius, maxRadius := 1, max(max(x, y), max(len(m)-1-x, len(m)-1-y))
+		minSteps := MaxInt
+		r := minRadius
+		for r = minRadius; r <= maxRadius; r *= 2 {
+			containsZero := containsZero(sumGraph, x, y, r)
+			if containsZero {
+				minSteps = findMinStepsAround(m, x, y, r)
+				if minSteps > r {
+					continue
+				}
+				return minSteps, r
 			}
 		}
-	}
-	return minSteps
-}
-
-func containsZero(sumGraph [][]int, x, y int, radius int) bool {
-	xMin, yMin, xMax, yMax := x-radius, y-radius, x+radius, y+radius
-	if xMin < 0 {
-		xMin = 0
-	}
-	if yMin < 0 {
-		yMin = 0
-	}
-	if xMax > len(sumGraph)-1 {
-		xMax = len(sumGraph) - 1
-	}
-	if yMax > len(sumGraph)-1 {
-		yMax = len(sumGraph) - 1
+		if maxRadius != r {
+			minSteps = findMinStepsAround(m, x, y, maxRadius)
+		}
+		return minSteps, r
 	}
 
-	sumAllNotZeros := (xMax - xMin) * (yMax - yMin + 1)
-	sum := sumAllNotZeros
-	if 0 == xMin && 0 == yMin {
-		sum = sumGraph[xMax][yMax]
-	} else if 0 == xMin && yMin > 0 {
-		sum = sumGraph[xMax][yMax] - sumGraph[xMax - 1][yMin - 1]
-	} else if xMin > 0 && 0 == yMin {
-		sum = sumGraph[xMax][yMax] - sumGraph[xMin - 1][yMax - 1]
-	} else {
-		sum = sumGraph[xMax][yMax] - sumGraph[xMin][yMax] - sumGraph[xMax][yMin] + sumGraph[xMin][yMin]
-	}
-	return sum < sumAllNotZeros
-}
-
-func findMinStepsAround(m, sumGraph [][]int, x, y int, len int) int {
-	return 1
-}
-
-func sumGraph(m [][]int) [][]int {
-	sumGraph := make([][]int, len(m))
-	for i := 0; i < len(m); i++ {
-		sumGraph[i] = make([]int, len(m[i]))
-		for j := 0; j < len(m[i]); j++ {
-			sumGraph[i][j] = 0
-			if 0 == j && 0 == i {
-				sumGraph[i][j] = m[i][j]
-			} else if 0 == j && i > 0 {
-				sumGraph[i][j] = sumGraph[i-1][j] + m[i][j]
-			} else if j > 0 && 0 == i {
-				sumGraph[i][j] = sumGraph[i][j-1] + m[i][j]
-			} else {
-				sumGraph[i][j] = sumGraph[i][j-1] + sumGraph[i-1][j] - sumGraph[i-1][j-1] + m[i][j]
+	sumGraph := func(m [][]int) [][]int {
+		sumGraph := make([][]int, len(m))
+		for i := range m {
+			sumGraph[i] = make([]int, len(m[i]))
+			for j := range m[i] {
+				sumGraph[i][j] = 0
+				if 0 == j && 0 == i {
+					sumGraph[i][j] = m[i][j]
+				} else if 0 == j && i > 0 {
+					sumGraph[i][j] = sumGraph[i-1][j] + m[i][j]
+				} else if j > 0 && 0 == i {
+					sumGraph[i][j] = sumGraph[i][j-1] + m[i][j]
+				} else {
+					sumGraph[i][j] = sumGraph[i][j-1] + sumGraph[i-1][j] - sumGraph[i-1][j-1] + m[i][j]
+				}
 			}
 		}
+		return sumGraph
 	}
-	return sumGraph
-}
 
-func updateMatrix(m [][]int) [][]int {
-	sumGraph := sumGraph(m)
-	contains := containsZero(sumGraph, 5, 5, 2)
-	fmt.Println(contains)
-	return sumGraph
-}
-
-func print(m [][]int) {
-	for i := 0; i < len(m); i++ {
-		for j := 0; j < len(m[i]); j++ {
-			if len(m[i])-1 == j {
-				fmt.Println(m[i][j])
-			} else {
-				fmt.Printf("%d ", m[i][j])
-			}
+	sumG := sumGraph(m)
+	updated := make([][]int, len(m))
+	for i := range m {
+		updated[i] = make([]int, len(m[i]))
+		for j := range m[i] {
+			minSteps, _ := findMinStepsWithScalingRadius(m, sumG, i, j)
+			updated[i][j] = minSteps
 		}
 	}
-}
-
-func main() {
-	var m [][]int
-	for i := 0; i < 10; i++ {
-		var r []int
-		for j := 0; j < 10; j++ {
-			r = append(r, rand.Intn(2))
-		}
-		m = append(m, r)
-	}
-	print(m)
-	m = updateMatrix(m)
-	fmt.Println("-----------------------")
-	print(m)
+	return updated
 }
